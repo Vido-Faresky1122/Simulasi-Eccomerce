9e639ed0a08fd91a29b5634989eaffec1240d329
Vido-Faresky <vidogamingmax@gmail.com>
2026-03-18 10:17:56 +0700

    feat: implement cart page

--- app/views/cart.php
+++ app/views/cart.php
@@ -1,11 +1,152 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Product Detail</title>
+  <link rel="stylesheet" href="/css/output.css">
 </head>
-<body>
+  
+
+
+<body class="bg-gray-100 min-h-screen font-sans flex flex-col">
+
+ <header class="bg-indigo-600 shadow-sm">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between items-center h-16">
+
+        <div class="shrink-0">
+          <a href="/" class="text-2xl font-bold text-white">Sudi Mart</a>
+        </div>
+        
+        <div class="flex items-center space-x-4">
+          <a href="#" class="hover:underline text-m text-white">About us</a>
+        </div>
+      </div>
+    </div>
+  </header>
+  
+<main class="grow flex flex-col items-center justify-center">
+
+<div class="w-5xl mx-auto">
     
+    <h1 class="text-2xl font-semibold mb-4">Keranjang</h1>
+    <hr class="mb-6">
+
+    <div class="bg-white rounded-lg shadow p-4 flex items-center justify-between mb-4">
+
+      <div class="flex items-center gap-4">
+        <input type="checkbox" class="w-5 h-5">
+
+        <img src="/assets/penkenko.png" class="w-20 h-20 object-cover border">
+
+        <div>
+          <h2 class="font-medium">Easy Gel Premium 1.0mm</h2>
+          <p class="text-sm text-gray-500">kenko</p>
+        </div>
+      </div>
+
+      <div class="flex items-center gap-6">
+        <p class="text-sm">Unit</p>
+
+        <div class="flex items-center gap-2">
+          <button class="w-8 h-8 border rounded-full flex items-center justify-center hover:cursor-pointer hover:bg-indigo-100">-</button>
+          <span>1</span>
+          <button class="w-8 h-8 border rounded-full flex items-center justify-center hover:cursor-pointer hover:bg-indigo-100">+</button>
+        </div>
+
+        <button class="bg-indigo-500 text-white px-4 py-1 rounded">
+          Delete
+        </button>
+      </div>
+
+    </div>
+
+    <div class="bg-white rounded-lg shadow p-4 flex items-center justify-between mb-4">
+
+      <div class="flex items-center gap-4">
+        <input type="checkbox" class="w-5 h-5">
+
+        <img src="/assets/penkenko.png" class="w-20 h-20 object-cover border">
+
+        <div>
+          <h2 class="font-medium">Tombow Mono CF Correction Tape CT-CF5</h2>
+          <p class="text-sm text-gray-500">Tombow</p>
+        </div>
+      </div>
+
+      <div class="flex items-center gap-6">
+        <p class="text-sm">Unit</p>
+
+        <div class="flex items-center gap-2">
+          <button class="w-8 h-8 border rounded-full flex items-center justify-center hover:cursor-pointer hover:bg-indigo-100">-</button>
+          <span>1</span>
+          <button class="w-8 h-8 border rounded-full flex items-center justify-center hover:cursor-pointer hover:bg-indigo-100">+</button>
+        </div>
+
+        <button class="bg-indigo-500 text-white px-4 py-1 rounded">
+          Delete
+        </button>
+      </div>
+
+    </div>
+
+    <div class="bg-white rounded-lg shadow p-4 flex items-center justify-between">
+
+      <div class="flex items-center gap-4">
+        <input type="checkbox" class="w-5 h-5">
+
+        <img src="/assets/penkenko.png" class="w-20 h-20 object-cover border">
+
+        <div>
+          <h2 class="font-medium">Schneider One Business Rollerball</h2>
+          <p class="text-sm text-gray-500">Schneider</p>
+        </div>
+      </div>
+
+      <div class="flex items-center gap-6">
+        <p class="text-sm">Unit</p>
+
+        <div class="flex items-center gap-2">
+          <button class="w-8 h-8 border rounded-full flex items-center justify-center hover:cursor-pointer hover:bg-indigo-100">-</button>
+          <span>1</span>
+          <button class="w-8 h-8 border rounded-full flex items-center justify-center hover:cursor-pointer hover:bg-indigo-100">+</button>
+        </div>
+
+        <button class="bg-indigo-500 text-white px-4 py-1 rounded">
+          Delete
+        </button>
+      </div>
+
+    </div>
+
+  </div>
+</main>
+
+  <footer class="mt-16 bg-indigo-700 text-white py-10 px-10">
+    <div class="max-w-5xl mx-auto grid md:grid-cols-2 gap-10">
+
+      <div>
+        <h2 class="text-lg font-semibold mb-3">About US</h2>
+        <p class="text-sm text-indigo-100">
+          Sudi Mart adalah mitra andalan kebutuhan warga sekolah,
+          menyediakan berbagai alat tulis, keperluan belajar,
+          serta layanan pendukung sekolah yang cepat dan berkualitas.
+        </p>
+      </div>
+
+      <div>
+        <h2 class="text-lg font-semibold mb-3">Contact Us</h2>
+        <p class="text-sm text-indigo-100">
+          IG : @sudi_martt <br>
+          TT : sm.mart <br>
+          X : sudi-M <br>
+          WA : +62 812 5609 8744
+        </p>
+      </div>
+
+    </div>
+  </footer>
+
 </body>
 </html>
