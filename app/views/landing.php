241dd5c36e567b40cf2d77d63e79494d3008fb6e
Vido-Faresky <vidogamingmax@gmail.com>
2026-04-29 09:19:35 +0700

    apalah

--- app/views/landing.php
+++ app/views/landing.php
@@ -2,12 +2,57 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome - SudiMart</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Cart</title>
+  <link rel="stylesheet" href="/css/output.css">
 </head>
 
-<body>
+
+
+<body class="bg-gray-100 min-h-screen font-sans flex flex-col gap-15 bg-linear-to-b from-[#4C128B] to-[#814FBE]">
+
+  <header class="bg-[#4C128B] shadow-sm">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between items-center h-16">
+
+        <div class="shrink-0">
+          <a href="/" class="text-2xl font-bold text-yellow-300">Sudi Mart</a>
+        </div>
+
+        <div class="flex items-center space-x-4">
+          <a href="#" class="hover:underline text-m text-yellow-300">About us</a>
+        </div>
+      </div>
+    </div>
+</header>
+
+
+
+  <footer class="mt-16 bg-[#4C128B] text-yellow-300 py-10 px-10">
+    <div class="max-w-5xl mx-auto grid md:grid-cols-2 gap-10">
+
+      <div>
+        <h2 class="text-lg font-semibold mb-3">About US</h2>
+        <p class="text-sm text-yellow-300">
+          Sudi Mart adalah mitra andalan kebutuhan warga sekolah,
+          menyediakan berbagai alat tulis, keperluan belajar,
+          serta layanan pendukung sekolah yang cepat dan berkualitas.
+        </p>
+      </div>
+
+      <div>
+        <h2 class="text-lg font-semibold mb-3">Contact Us</h2>
+        <p class="text-sm text-yellow-300">
+          IG : @sudi_martt <br>
+          TT : sm.mart <br>
+          X : sudi-M <br>
+          WA : +62 812 5609 8744
+        </p>
+      </div>
+
+    </div>
+  </footer>
 
 </body>
 
